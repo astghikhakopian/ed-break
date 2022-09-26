1953d56f9e81a3b198d785bd330dd4a9a4ed0210
Astghik Hakopian <astghik.hakopian@gmail.com>
2022-09-26 12:09:25 +0400

    Initial commit

---
+++ Ed-Break/App/Ed_BreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ed_BreakApp.swift
+//  Ed-Break
+//
+//  Created by Astghik Hakopian on 26.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Ed_BreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
